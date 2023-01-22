a911c2fba48ffea80e23ad6693cf92487ae55175
Tiago2 <tiago8204@gmail.com>
2023-01-22 19:02:53 -0300

    test

--- test cmake/test cmake.cpp
+++ test cmake/test cmake.cpp
@@ -1,1 +1,39 @@
-﻿// test cmake.cpp: define o ponto de entrada para o aplicativo.//#include "hello.h"#include "test cmake.h"using namespace std;class Vector2d{public:	void setX(float x_)	{		x = x_*2;	}	float getX(void)	{		return x;	}private:	float x, y;};int main(){	cout << "Bem-vindo" << endl;		Helloworld aa; // Classe Helloworld 	aa.helo2();	Vector2d vetor; // testando classe Vector 2D	vetor.setX(22);	cout << vetor.getX();	return 0; // indica que o programa terminou}
+﻿// test cmake.cpp: define o ponto de entrada para o aplicativo.
+//
+#include "hello.h"
+#include "test cmake.h"
+
+using namespace std;
+
+class Vector2d
+{
+public:
+	void setX(float x_)
+	{
+		x = x_*2;
+	}
+	float getX(void)
+	{
+		return x;
+	}
+
+
+private:
+	float x, y;
+};
+
+int main()
+{
+	cout << "Bem-vindo" << endl;
+	
+	Helloworld aa; // Classe Helloworld 
+	aa.helo2();
+
+	Vector2d vetor; // testando classe Vector 2D
+	vetor.setX(22);
+	cout << vetor.getX();
+
+	return 0; // indica que o programa terminou
+}
+
+
